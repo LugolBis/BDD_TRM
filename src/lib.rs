9639b794497007d8652762190fc5945a76f47924
LugolBis <ld.desmares@gmail.com>
2024-10-18 18:13:37 +0200

    add a README

--- src/lib.rs
+++ src/lib.rs
@@ -91,13 +91,13 @@ impl Plan {
                         if value.starts_with("#") && (value.find("##")==None) { Some(format!("##{}_{}", entity_2, &value[1..]).to_string()) }
                         else { None } }).collect();
                 },
-                None=> panic!("The entity : {} doesn't exist in 'Plan.entities'.",entity_2)
+                None=> panic!("The entity : {} isn't in the 'Entities' arg from the method '.translate()'.",entity_2)
             }
             attribute.extend_from_slice(&relationship.1.0);
             if let Some(vector_1) = self.tables.get_mut(&entity_1) {
                 vector_1.extend_from_slice(&attribute);
             } else {
-                panic!("The entity : {} doesn't exist in 'Plan.entities'.",entity_1)
+                panic!("The entity : {} isn't in the 'Entities' arg from the method '.translate()'.",entity_1)
             }
         } else {
             match self.tables.get(&entity_1) {
@@ -107,13 +107,13 @@ impl Plan {
                         if value.starts_with("#") && (value.find("##")==None) { Some(format!("##{}_{}", entity_1, &value[1..]).to_string()) }
                         else { None } }).collect();
                 },
-                None=> panic!("The entity : {} doesn't exist in 'Plan.entities'.",entity_1)
+                None=> panic!("The entity : {} isn't in the 'Entities' arg from the method '.translate()'.",entity_1)
             }
             attribute.extend_from_slice(&relationship.1.0);
             if let Some(vector_2) = self.tables.get_mut(&entity_2) {
                 vector_2.extend_from_slice(&attribute);
             } else {
-                panic!("The entity : {} doesn't exist in 'Plan.entities'.",entity_2)
+                panic!("The entity : {} isn't in the 'Entities' arg from the method '.translate()'.",entity_2)
             }
         }
     }
@@ -175,115 +175,3 @@ impl Plan {
         }
     }
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_one_to_one_same() {
-        let entities_var = Entities::from(vec![
-            ("Voiture",vec!["#NVEH","Model","Année"]),
-            ("Garage", vec!["km","etat","#adresse"]) ]);
-        let relationships_var = Relationships::from(vec![
-            ("Stocké",(vec!["date"],(false,false,"Voiture","Voiture")))
-        ]);
-        let mut plan_var = Plan::new();
-        plan_var.translate(entities_var,relationships_var);
-        println!("{:?}",plan_var.tables);
-        let mut result:HashMap<String,Vec<String>> = HashMap::new();
-        result.insert("Voiture".to_string(),vec!["#NVEH".to_string(), "Model".to_string(), "Année".to_string(), "date".to_string()]);
-        result.insert("Garage".to_string(),vec!["km".to_string(), "etat".to_string(), "#adresse".to_string()]);
-        assert!(plan_var.tables==result);
-    }
-
-    #[test]
-    fn test_one_to_one_other() {
-        let entities_var = Entities::from(vec![
-            ("Voiture",vec!["#NVEH","Model","Année"]),
-            ("Garage", vec!["km","etat","#adresse"]) ]);
-        let relationships_var = Relationships::from(vec![
-            ("Stocké",(vec!["date"],(false,false,"Garage","Voiture")))
-        ]);
-        let mut plan_var = Plan::new();
-        plan_var.translate(entities_var,relationships_var);
-        println!("{:?}",plan_var.tables);
-        let mut result:HashMap<String,Vec<String>> = HashMap::new();
-        result.insert("Voiture".to_string(),vec!["#NVEH".to_string(), "Model".to_string(), "Année".to_string(), "##Garage_adresse".to_string(), "date".to_string()]);
-        result.insert("Garage".to_string(),vec!["km".to_string(), "etat".to_string(), "#adresse".to_string()]);
-        assert!(plan_var.tables==result);
-    }
-
-    #[test]
-    fn test_one_to_many() {
-        let entities_var = Entities::from(vec![
-            ("Voiture",vec!["#NVEH","Model","Année"]),
-            ("Garage", vec!["km","etat","#adresse"]) ]);
-        let relationships_var = Relationships::from(vec![
-            ("Stocké",(vec!["date"],(false,true,"Garage","Voiture")))
-        ]);
-        let mut plan_var = Plan::new();
-        plan_var.translate(entities_var,relationships_var);
-        println!("{:?}",plan_var.tables);
-        let mut result:HashMap<String,Vec<String>> = HashMap::new();
-        result.insert("Voiture".to_string(),vec!["#NVEH".to_string(), "Model".to_string(), "Année".to_string()]);
-        result.insert("Garage".to_string(),vec!["km".to_string(), "etat".to_string(), "#adresse".to_string(), "##Voiture_NVEH".to_string(), "date".to_string()]);
-        assert!(plan_var.tables==result);
-    }
-
-    #[test]
-    fn test_many_to_one() {
-        let entities_var = Entities::from(vec![
-            ("Voiture",vec!["#NVEH","Model","Année"]),
-            ("Garage", vec!["km","etat","#adresse"]) ]);
-        let relationships_var = Relationships::from(vec![
-            ("Stocké",(vec!["date"],(true,false,"Garage","Voiture")))
-        ]);
-        let mut plan_var = Plan::new();
-        plan_var.translate(entities_var,relationships_var);
-        println!("{:?}",plan_var.tables);
-        let mut result:HashMap<String,Vec<String>> = HashMap::new();
-        result.insert("Voiture".to_string(),vec!["#NVEH".to_string(), "Model".to_string(), "Année".to_string(), "##Garage_adresse".to_string(), "date".to_string()]);
-        result.insert("Garage".to_string(),vec!["km".to_string(), "etat".to_string(), "#adresse".to_string()]);
-        assert!(plan_var.tables==result);
-    }
-
-    #[test]
-    fn test_one_to_many_same() {
-        let entities_var = Entities::from(vec![
-            ("Voiture",vec!["#NVEH","Model","Année"]),
-            ("Garage", vec!["km","etat","#adresse"]) ]);
-        let relationships_var = Relationships::from(vec![
-            ("Stocké",(vec!["date"],(false,true,"Voiture","Voiture")))
-        ]);
-        let mut plan_var = Plan::new();
-        plan_var.translate(entities_var,relationships_var);
-        println!("{:?}",plan_var.tables);
-        let mut result:HashMap<String,Vec<String>> = HashMap::new();
-        result.insert("Voiture".to_string(),vec!["#NVEH".to_string(), "Model".to_string(), "Année".to_string(), "##Voiture_NVEH".to_string(), "date".to_string()]);
-        result.insert("Garage".to_string(),vec!["km".to_string(), "etat".to_string(), "#adresse".to_string()]);
-        assert!(plan_var.tables==result);
-    }
-
-    #[test]
-    fn test_many_to_many() {
-        let entities_var = Entities::from(vec![
-            ("Voiture",vec!["#NVEH","Model","Année"]),
-            ("Garage", vec!["km","etat","#adresse"]) ]);
-        let relationships_var = Relationships::from(vec![
-            ("Stocké",(vec!["date"],(true,true,"Garage","Voiture")))
-        ]);
-        let mut plan_var = Plan::new();
-        plan_var.translate(entities_var,relationships_var);
-        println!("{:?}",plan_var.tables);
-        let mut result:HashMap<String,Vec<String>> = HashMap::new();
-        result.insert("Voiture".to_string(),vec!["#NVEH".to_string(), "Model".to_string(), "Année".to_string()]);
-        result.insert("Garage".to_string(),vec!["km".to_string(), "etat".to_string(), "#adresse".to_string()]);
-        result.insert("Stocké".to_string(),vec!["##Garage_adresse".to_string(), "##Voiture_NVEH".to_string(), "date".to_string()]);
-        assert!(plan_var.tables==result);
-    }
-}
-
-// README
-// This crate permite to convert an Entity/Relationship (E/R) to a Relationnal model.
-// DISCLAIMER : It only support E/R model where there is only binary relationship.
